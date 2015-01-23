bc0887f444aa514d304ae38bbc3dccbbb7ce19e3
mjknowles23 <mjknowles23@gmail.com>
2015-01-22 21:37:50 -0600

    added database project and reminders folder to solution

---
+++ SoundTrackr.Db/CustomScripts/SongCoord/INSERT/Insert3Coords.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO SongCoord (coord)
+VALUES (geography::STGeomFromText('POINT(33.656415 -86.527409)', 4326));
+INSERT INTO SongCoord (coord)
+VALUES (geography::STGeomFromText('POINT(34.787571 -86.939557)', 4326));
+INSERT INTO SongCoord (coord)
+VALUES (geography::STGeomFromText('POINT(36.145660 -86.795120)', 4326));
---
+++ SoundTrackr.Db/dbo/Tables/SongCoord.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SongCoord] (
+    [songCoordId] INT               IDENTITY (1, 1) NOT NULL,
+    [songId]      INT               NULL,
+    [coord]       [sys].[geography] NULL,
+    PRIMARY KEY CLUSTERED ([songCoordId] ASC)
+);
+
