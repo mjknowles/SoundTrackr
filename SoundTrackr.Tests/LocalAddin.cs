2a02b193fbcdeea920b8ce8ef99ed4d0dd5de3ef
mjknowles <mjknowles23@gmail.com>
2015-04-05 12:18:37 -0500

    Adding AutoFixture class to tests.

---
+++ SoundTrackr.Tests/LocalAddin.cs
@@ -0,0 +1,9 @@
+using NUnit.Core.Extensibility;
+
+namespace SoundTrackr.Web.Tests
+{
+    [NUnitAddin]
+    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    {   
+    }
+}
--- SoundTrackr.Tests/Repositories/GenericRepoTests.cs
+++ SoundTrackr.Tests/Repositories/GenericRepoTests.cs
@@ -9,6 +9,8 @@ using SoundTrackr.Repository.Repositories;
 using System.Collections.Generic;
 using SoundTrackr.Common.Domain;
 using SoundTrackr.Domain.Entities.Track;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
 
 namespace SoundTrackr.Web.Tests.Repositories
 {
@@ -27,27 +29,28 @@ namespace SoundTrackr.Web.Tests.Repositories
     [TestClass]
     public class GenericRepoTests
     {
-        private Mock<IUnitOfWork> _mockUnitOfWork;
-        private Mock<IDbContextFactory> _mockDbContextFactory;
+        //private Mock<IUnitOfWork> _mockUnitOfWork;
+        //private Mock<IDbContextFactory> _mockDbContextFactory;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            _mockUnitOfWork = new Mock<IUnitOfWork>();
-            _mockDbContextFactory = new Mock<IDbContextFactory>();
+            //mockUnitOfWork = new Mock<IUnitOfWork>();
+            //_mockDbContextFactory = new Mock<IDbContextFactory>();
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
-            _mockUnitOfWork.VerifyAll();
-            _mockDbContextFactory.VerifyAll();
+            //_mockUnitOfWork.VerifyAll();
+            //_mockDbContextFactory.VerifyAll();
         }
 
         [TestMethod]
-        public void GenericRepo_FindById()
+        public void GenericRepo_FindById_ExistingID_ObjectIsReturned()
         {
-            var data = new List<TestEntity> 
+
+            /*var data = new List<TestEntity> 
             { 
                 new TestEntity { Id = 0 }, 
                 new TestEntity { Id = 1 }, 
@@ -64,12 +67,25 @@ namespace SoundTrackr.Web.Tests.Repositories
             var mockContext = new Mock<DbContext>();
             mockContext.Setup(c => c.Set<TestEntity>()).Returns(mockSet.Object);
 
-            _mockDbContextFactory.Setup(x => x.Create()).Returns(mockContext.Object);
+            //_mockDbContextFactory.Setup(x => x.Create()).Returns(mockContext.Object);
 
-            TestGenericRepo testRepo = new TestGenericRepo(_mockUnitOfWork.Object, _mockDbContextFactory.Object);
+            //TestGenericRepo testRepo = new TestGenericRepo(_mockUnitOfWork.Object, _mockDbContextFactory.Object);
+            */
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var data = fixture.CreateMany<TestEntity>().ToList<TestEntity>().AsQueryable<TestEntity>();
+            var mockSet = fixture.Freeze<Mock<DbSet<TestEntity>>>();
+            mockSet.As<IQueryable<TestEntity>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<TestEntity>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<TestEntity>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<TestEntity>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var contextFactory = fixture.Freeze<Mock<IDbContextFactory>>();
+            contextFactory.Setup(x => x.Create()).Returns();
+            var sut = fixture.Create<TestGenericRepo>();
 
             var expectedModel = new Track { Id = 1 };
-            var actualModel = testRepo.FindById(1);
+            var actualModel = sut.FindById(1);
 
             Assert.AreEqual(expectedModel.Id, actualModel.Id);
         }
