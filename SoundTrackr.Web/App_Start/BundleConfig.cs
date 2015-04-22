2d07a40e2d9760c81a80a50894d13b29977c10ae
mjknowles <mjknowles23@gmail.com>
2015-04-21 23:10:41 -0500

    Added example mapbox to project.

--- SoundTrackr.Web/App_Start/BundleConfig.cs
+++ SoundTrackr.Web/App_Start/BundleConfig.cs
@@ -9,16 +9,16 @@ namespace SoundTrackr.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/vendor/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/vendor/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/vendor/bootstrap.js",
+                      "~/Scripts/vendor/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ SoundTrackr.Web/Controllers/TracksController.cs
@@ -0,0 +1,106 @@
+﻿using SoundTrackr.Service.Messaging;
+using SoundTrackr.Service.Messaging.Track;
+using SoundTrackr.Service.Track;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using SoundTrackr.Web.Helpers;
+using System.Web.Mvc;
+using SoundTrackr.Service.DTOs;
+
+namespace SoundTrackr.Web.Controllers.API
+{
+    [System.Web.Http.RoutePrefix("api/tracks")]
+    public class TracksController : ApiController
+    {
+        private readonly ITrackService _trackService;
+
+        public TracksController(ITrackService trackService)
+        {
+            if (trackService == null) throw new ArgumentNullException("Track service in track controller");
+            _trackService = trackService;
+        }
+
+        // GET: api/gifentries
+        [System.Web.Http.Route("")]
+        public HttpResponseMessage GetTracks()
+        {
+            ServiceResponseBase resp = _trackService.GetTracks(new GetTracksRequest(User.Identity.GetUserId()));
+            return Request.BuildResponse(resp);
+        }
+
+        // GET: api/gifentries
+        [System.Web.Http.Route("{userName}")]
+        public HttpResponseMessage GetTracks(string userName)
+        {
+            ServiceResponseBase resp = _trackService.GetTracks(new GetTracksRequest { UserName = userName });
+            return Request.BuildResponse(resp);
+        }
+    }
+}
+
+namespace SoundTrackr.Web.Controllers.MVC
+{
+    public class TracksController : Controller
+    {
+        private readonly ITrackService _trackService;
+
+        public TracksController(ITrackService trackService)
+        {
+            if (trackService == null) throw new ArgumentNullException("Track service in track controller");
+            _trackService = trackService;
+        }
+
+        // GET: Tracks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Tracks/Details/5
+        public ActionResult Details(int? id)
+        {
+            return View(new TrackDTO());
+        }
+
+        // GET: Tracks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Tracks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrackDTO trackDTO = new TrackDTO();
+            if (trackDTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new TrackDTO());
+        }
+
+        // GET: Tracks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrackDTO trackDTO = new TrackDTO();
+            if (trackDTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trackDTO);
+        }
+    }
+}
