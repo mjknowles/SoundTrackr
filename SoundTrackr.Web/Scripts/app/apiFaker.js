2f798601dee1bcb3200087dec8447e216930a434
mjknowles <mjknowles23@gmail.com>
2015-05-18 20:15:26 -0500

    Created basic two column layout.

---
+++ SoundTrackr.Web/Scripts/app/apiFaker.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+(function (ng) {
+    var tracks = [
+      { title: 'Washington D.C.', id: 1 },
+      { title: 'Nash to Bham', id: 2 },
+      { title: 'Around the Block', id: 3 }
+    ];
+
+    ng.module('soundTrackrApp')
+      .config(['$provide', function ($provide) {
+          $provide.decorator('$httpBackend', ng.mock.e2e.$httpBackendDecorator);
+      }]).run(['$httpBackend', function ($httpBackend) {
+          $httpBackend.whenGET('/api/tracks/testuser').respond(tracks);
+      }]);
+})(angular);
--- SoundTrackr.Web/Scripts/app/service.js
+++ SoundTrackr.Web/Scripts/app/service.js
@@ -6,7 +6,12 @@ var soundTrackrServices = angular.module('soundTrackrServices', ['ngResource']);
 
 soundTrackrServices.factory('Track', ['$resource',
   function ($resource) {
-      return $resource('/api/tracks/:trackId', {}, {
-          query: { method: 'GET', params: {trackId:''}, isArray:true }
-      });
+      return {
+          track: $resource('/api/tracks/:trackId', {}, {
+              query: { method: 'GET', params: { trackId: '' }, isArray: false }
+          }),
+          tracks: $resource('/api/tracks/:userName', {}, {
+              query: { method: 'GET', params: { userName: 'testuser' }, isArray: false }
+          }),
+      };
   }]);
