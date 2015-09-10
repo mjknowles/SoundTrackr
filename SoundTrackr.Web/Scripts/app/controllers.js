6ea8bb70e9b789b1281dfc139c2e8e68dabd0e96
mjknowles <mjknowles23@gmail.com>
2015-09-09 23:05:44 -0500

    Bug fix - app now auto zooms into the first point on a track when user clicks it. Broken after updating json to add more details.

--- SoundTrackr.Web/Scripts/app/controllers.js
+++ SoundTrackr.Web/Scripts/app/controllers.js
@@ -31,10 +31,11 @@ soundTrackrControllers.controller('TracksCtrl', ['$scope', 'TrackAPI', 'UserTrac
       });
 
       var myLayer = L.mapbox.featureLayer().addTo(map);
+
       $scope.getTrack = function getTrack(id) {
           TrackAPI.track.query({trackId: id}, function (resp) {
               myLayer.setGeoJSON(resp.GeoJsonLayers[0].GeoJsonObject);
-              map.setView(new Array(resp.GeoJsonLayers[0].GeoJsonObject.features[0].geometry.coordinates[1], resp.GeoJson.features[0].geometry.coordinates[0]), 14);
+              map.setView(new Array(resp.GeoJsonLayers[0].GeoJsonObject.features[0].geometry.coordinates[1], resp.GeoJsonLayers[0].GeoJsonObject.features[0].geometry.coordinates[0]), 14);
           });
       };
   }]);
